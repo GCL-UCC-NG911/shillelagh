7918462d29196491b14a6eae3e839e51d446ff2a
Geert Fieremans <geert.fieremans@atos.net>
2023-04-12 10:43:14 -0400

    Remove changes to test_ngls_alchemy_dialect.py

--- test/test_ngls_alchemy_dialect.py
+++ test/test_ngls_alchemy_dialect.py
@@ -38,8 +38,7 @@ print(inspector.get_table_names())
 
 print("get data")
 with engine.connect() as connection:
-    #result = connection.execute(text("SELECT * FROM busiest_hour WHERE call_type IN ('911', 'admin')"))
-    result = connection.execute(text("SELECT * FROM busiest_hour"))
+    result = connection.execute(text("SELECT * FROM busiest_hour WHERE call_type IN ('911', 'admin')"))
     for row in result:
         print(row)
 
