635ffe24227cb8e7999dd53b3ef7964feb70a0a2
LUANA POVROZNIK (ext) <luana.povroznik.external@atos.net>
2024-01-30 15:05:47 -0300

    add log

--- src/shillelagh/backends/apsw/dialects/ngls.py
+++ src/shillelagh/backends/apsw/dialects/ngls.py
@@ -230,6 +230,7 @@ class NglsReports:
                 filters = (
                     [FILTER_NAME_TO_FILTER[x] for x in filters] if filters else None
                 )
+                _logger.info(f"Class name test: {CLASS_NAME_TO_CLASS[class_name]}")
                 columns_dict[column["column_name"]] = CLASS_NAME_TO_CLASS[class_name](
                     filters=filters,
                     order=field.get("order", Order.NONE),
