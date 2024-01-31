c11b448a772327b058bea7c6f12550770c6c6de7
LUANA POVROZNIK (ext) <luana.povroznik.external@atos.net>
2024-01-31 13:43:39 -0300

    update

--- src/shillelagh/backends/apsw/dialects/ngls.py
+++ src/shillelagh/backends/apsw/dialects/ngls.py
@@ -181,10 +181,10 @@ class NglsReports:
         url = self.url(tablename)
         if not params:
             params = {"superset": "true"}
-            if tablename == 'test':
-                params = {"superset": "true", "from": "2023-11-07T00:00:00.000Z", "to": "2024-01-30T23:59:00.000Z"}
         else:
             params["superset"] = "true"
+        if tablename == 'test':
+            params = {"superset": "true", "from": "2023-11-07T00:00:00.000Z", "to": "2024-01-30T23:59:00.000Z"}
         _logger.info(f"Get report from NGLS: GET {url} {json.dumps(params)}")
         response = requests.get(
             url,
