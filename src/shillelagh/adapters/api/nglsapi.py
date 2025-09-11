c4aa39492e557e038eecfe34fa0536b1bc498447
lpovroznik <luana.povroznik@atos.net>
2025-09-11 14:45:44 -0300

    Change direction values

--- src/shillelagh/adapters/api/nglsapi.py
+++ src/shillelagh/adapters/api/nglsapi.py
@@ -80,7 +80,7 @@ class NglsAPI(Adapter):
                 ["AAC Notice"],
                 ["monitoringCall"],
             ],
-            "direction_tags": [["incoming"], ["outgoing"]],
+            "direction_tags": [["Inbound"], ["Outbound"]],
             # fmt: on
             "seq_nrs": [[str(x).zfill(4)] for x in range(1, 1001)],
         }
