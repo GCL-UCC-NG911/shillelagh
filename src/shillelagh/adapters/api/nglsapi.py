a04b70bb58f3d55ec5883cb64fb56943e331b534
Luana Povroznik <luana.povroznik@atos.net>
2026-01-08 16:15:53 -0300

    NGLS: Request to Implement User Friendly PSAP Names for Superset Reports (PTASK0010783)

--- src/shillelagh/adapters/api/nglsapi.py
+++ src/shillelagh/adapters/api/nglsapi.py
@@ -108,7 +108,7 @@ class NglsAPI(Adapter):
                 raise InternalError(
                     "Error while getting data from ngls-reporting service",
                 )
-            data = [[x.get("id")] for x in result]
+            data = [[x.get("displayName", x.get("id"))] for x in result]
         else:
             params = self.set_params(bounds)
             result = self.nglsreports.get(self.table, params)
